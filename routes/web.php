ff4f96d907aba7506464e23f09da78b9af9e73e9
oluwatobi <oluwatobiajayi001@gmail.com>
2025-09-22 15:23:19 +0100

    fixed

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Catch-all for React routes
+Route::get('{any}', function () {
+    return file_get_contents(public_path('index.html'));
+})->where('any', '^(?!api).*$');
+
